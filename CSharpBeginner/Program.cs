d713d51440780230e86aef8c14d4b5e0f3e39847
Melissa Astbury <melissaastbury@hotmail.com>
2021-03-22 15:06:04 +0000

    added in a if statement in a console application

---
+++ CSharpBeginner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int c = 4;
+            if (
+                (a + b + c > 10)
+
+                &&
+
+                (a == b)
+
+                )
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+        }
+    }
+}
